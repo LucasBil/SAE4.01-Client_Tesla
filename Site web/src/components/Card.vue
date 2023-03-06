f9e0229aea941fa3c9c6583283cc35f78f56085b
rondetp <pierre.rondet@etu.univ-smb.fr>
2023-03-06 13:57:13 +0100

    Renommage dossier

---
+++ Site web/src/components/Card.vue
@@ -0,0 +1,39 @@
+<script setup>
+    defineProps({
+        title: {
+            type: String,
+            required: true,
+            default: 'Card title'
+        },
+        resume: {
+            type: String,
+            required: true,
+            default: 'Lorem Poscriptum ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl nec ultricies lacinia, nisl nisl aliquam nisl, nec lacinia nisl nisl nec nisl.'
+        },
+        _button: {
+            type: String,
+            required: true,
+            default: 'Learn more'
+        },
+        _img: {
+            type: String,
+            required: true,
+            default: 'https://img1.ak.crunchyroll.com/i/spire1/d5ad9e0a13330935dd0905b2904b00041588258124_full.jpg'
+        },
+    })
+</script>
+
+<template>
+    <div class="card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+            <img class="object-cover h-full" :src="_img" alt="Album"/>
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">{{ title }}</h2>
+            <p>{{ resume }}</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">{{ _button }}</button>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ Site web/src/views/LoginView.vue
@@ -0,0 +1,36 @@
+<script setup></script>
+
+<template>
+    <div class="hero min-h-[calc(100vh-288px)] bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+        <div class="text-center lg:text-left">
+        <h1 class="text-5xl py-6 font-bold">Se connecter</h1>
+        </div>
+        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+            <div class="form-control">
+            <label class="label">
+                <span class="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="email" class="input input-bordered" />
+            </div>
+            <div class="form-control">
+            <label class="label">
+                <span class="label-text">Mot de passe</span>
+            </label>
+            <input type="password" placeholder="************" class="input input-bordered" />
+            <label class="label">
+                <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+            </label>
+            </div>
+            <div class="form-control mt-6">
+            <button class="btn btn-primary">Connecter</button>
+            <label class="label">
+                <a href="/createaccount" class="label-text-alt underline link link-hover">Créer un Compte</a>
+            </label>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+</template>
