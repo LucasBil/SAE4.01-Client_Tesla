cffbd448fabb8f840e95baf2caeecbbe89d7030d
LucasBil <lucas.billy@etu.univ-savoie.fr>
2023-03-13 17:31:04 +0100

    Home Page

--- Site web/src/views/HomeView.vue
+++ Site web/src/views/HomeView.vue
@@ -32,7 +32,10 @@
 </script>
 
 <template v-if="status_request">
-  <div class="p-6 w-full h-[500px]">
-    <Carousel :_imgs="_imgMemes" :hiden="{'number': true}"/>
+  <div class="w-full h-[calc(100vh-64px)] bg-[url(../assets/img/teslaHomePage.jpg)] text-base-100 bg-cover flex justify-center align-middle items-center">
+    <div class="border-8 gap-6 flex flex-col font-semibold justify-center items-center border-base-100 p-3 w-[85%] h-1/2">
+      <h1 class="text-4xl">Profiter de l'expérience Tesla</h1>
+      <p class="text-5xl text-center">L' <span class="text-6xl">avenir</span> et l'ingéniosité dans une <span class="text-6xl">voiture</span></p>
+    </div>
   </div>
 </template>
