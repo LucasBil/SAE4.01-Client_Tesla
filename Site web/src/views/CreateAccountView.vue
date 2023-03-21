3837f9220ac104598796e0177f3e0ed11988951f
LucasBil <lucas.billy@etu.univ-savoie.fr>
2023-03-21 17:39:26 +0100

    Post Cross Request pb ? Login avancer

--- Site web/src/views/CreateAccountView.vue
+++ Site web/src/views/CreateAccountView.vue
@@ -3,28 +3,11 @@
 
   // Stores
   import { request, controller } from '../stores';
-  import saves from '../stores/saves.js';
 
   // Composants
   import InputForm from '../components/InputForm.vue';
 
   let compte = ref({});
-  
-  function Post()
-  {
-    request().access();
-    onMounted(async () => {
-        controller().ComptesController.Post()
-        .then((response) => {
-            console.log(response.data);
-            request().success(response);
-        })
-        .catch((error) => {
-            request().error(error);
-            request().debug();
-        });
-    });
-  }
 
   request().access();
   onMounted(async () => {
@@ -38,6 +21,19 @@
           request().debug();
       });
   });
+
+  async function Post()
+  {
+    request().access();
+    await controller().ComptesController.Post(compte.value)
+    .then((response) => {
+        request().success(response);
+    })
+    .catch((error) => {
+        request().error(error);
+        request().debug();
+    });
+  }
 
   //function
   function ProAccount(event)
@@ -57,32 +53,6 @@
       document.querySelectorAll(".pro").forEach(item => {
         item.classList.add("hidden");
       })
-    }
-  }
-
-  function InputValidator(event) {
-    if(event.target.checkValidity() && event.target.value != "") {
-      if(event.target.classList.contains("input-error")) {
-        event.target.classList.remove("input-error","animate-pulse");
-      }
-      event.target.classList.add("input-success");
-    } 
-    else {
-      if(event.target.value == "" && !event.target.required) {
-        if(event.target.classList.contains("input-success")) {
-          event.target.classList.remove("input-success");
-        }
-        if(event.target.classList.contains("input-error")) {
-          event.target.classList.remove("input-error","animate-pulse");
-        }
-      }
-      else
-      {
-        if(event.target.classList.contains("input-success")) {
-          event.target.classList.remove("input-success");
-        }
-        event.target.classList.add("input-error","animate-pulse");
-      }
     }
   }
 
@@ -120,7 +90,6 @@
   }
 
   function formValidator(){
-    console.log(compte.value);
     let inputs = document.querySelectorAll("input");
     let select = document.querySelector("select");
     let error = false;
@@ -137,11 +106,16 @@
     if(error) {
       document.getElementById("msg_error").classList.remove("hidden");
     }
+    else
+    {
+      localStorage.user = compte.value;
+      Post();
+    }
   }
 </script>
 
 <template>
-  <form action="/" method="get" class="md:grid md:grid-cols-3">
+  <div class="md:grid md:grid-cols-3"> <!-- Form -->
     <div class="px-6 pt-6 md:p-6">
       <h1 class="text-2xl font-bold">Information Obligatoire</h1>
       <p class="py-3">Ces informations sont obligatoires pour la création d'un compte Tesla.</p>
@@ -156,11 +130,18 @@
           </select>
         </div>
         <div class="grid grid-cols-2 gap-3">
-          <InputForm _label="Nom" :_input="{type:'text',placeholder:'Nom',required:true}" />
-          <InputForm _label="Prénom" :_input="{type:'text',placeholder:'Prénom',required:true}" />
+          <div>
+            <h1>Nom : <span class="text-error">*</span></h1>
+            <InputForm @emit-value="compte.nomCompte = $event" :_input="{type:'text',placeholder:'Nom',required:true}" />
+          </div>
+          <div>
+            <h1>Prénom : <span class="text-error">*</span></h1>
+            <InputForm @emit-value="compte.prenomCompte = $event" :_input="{type:'text',placeholder:'Prénom',required:true}" />
+          </div>
         </div>
         <div class="flex flex-col gap-1">
-          <InputForm _label="Email" :_input="{type:'email',placeholder:'exemple@gmail.com',required:true}" />
+          <h1>Email : <span class="text-error">*</span></h1>
+          <InputForm @emit-value="compte.email = $event" :_input="{type:'email',placeholder:'exemple@gmail.com',required:true}" />
         </div>
         <div class="flex flex-col gap-1">
           <div class="grid grid-col grid-cols-10 gap-3">
@@ -169,11 +150,11 @@
               <span class="underline">i</span>
             </div>
           </div>
-          <input v-model="compte.motDePasse" @change="InputValidator($event);" pattern="^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,}$" type="password" autocomplete="true" placeholder="************" class="input input-bordered w-full" required/>
+          <InputForm @emit-value="compte.motDePasse = $event" :_input="{type:'password',pattern:'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*_-]).{8,}$',placeholder:'************',required:true}" />
         </div>
         <div class="flex flex-col gap-1">
           <h1>Confirmation mot de passe : <span class="text-error">*</span></h1>
-          <input @change="InputValidator($event);SamePwd($event)" pattern="^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,}$" type="password" autocomplete="true" placeholder="************" class="input input-bordered w-full" required/>
+          <InputForm @change="SamePwd($event)" :_input="{type:'password',pattern:'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*_-]).{8,}$',placeholder:'************',required:true}" />
         </div>
       </div>
     </div>
@@ -187,16 +168,18 @@
     <div class="col-span-2 m-3 p-6 rounded-xl shadow-xl ">
       <div class="flex flex-col gap-3">
         <div class="flex flex-col gap-1">
-          <InputForm _label="Numéro de téléphone" :_input="{type:'tel',placeholder:'+33000000000'}" />
+          <h1>Numéro de Téléphone :</h1>
+          <InputForm @emit-value="compte.numeroTelephone = $event" :_input="{type:'tel',placeholder:'+33000000000'}" />
         </div>
         <div class="grid grid-cols-3 gap-y-1 gap-3">
           <h1 class="col-span-3">Addresse :</h1>
-          <input v-model="compte.numeroRue" @change="InputValidator($event)" type="number" min="1" max="1000" placeholder="9" class="input input-bordered w-full" />
-          <input v-model="compte.nomRue" @change="InputValidator($event)" type="text" placeholder="rue des usines" class="input input-bordered w-full"/>
-          <input v-model="compte.ville" @change="InputValidator($event)" type="text" placeholder="Annecy" class="input input-bordered w-full"/>
+          <InputForm @emit-value="compte.numeroRue = $event" :_input="{type:'number',placeholder:'9',min:1,max:1000}" />
+          <InputForm @emit-value="compte.nomRue = $event" :_input="{type:'text',placeholder:'rue des usines'}" />
+          <InputForm @emit-value="compte.ville = $event" :_input="{type:'text',placeholder:'Annecy'}" />
         </div>
         <div class="flex flex-col gap-1">
-          <InputForm _label="Code Postal" :_input="{type:'text',placeholder:'74000'}" />
+          <h1 class="col-span-3">Code Postal :</h1>
+          <InputForm @emit-value="compte.codepostal = $event" :_input="{type:'text',placeholder:'74000'}" />
         </div>
       </div>
     </div>
@@ -212,19 +195,19 @@
     <div class="hidden col-span-2 m-3 p-6 rounded-xl shadow-xl  pro">
       <div class="flex flex-col gap-3">
         <div class="flex flex-col gap-1">
-          <InputForm _label="Nom de l'entreprise" :_input="{type:'text',placeholder:'Tesla'}" />
+          <InputForm @emit-value="compte.nomEntreprise = $event" _label="Nom de l'entreprise" :_input="{type:'text',placeholder:'Tesla'}" />
         </div>
         <div class="flex flex-col gap-1">
-          <InputForm _label="Numéro de TVA" :_input="{type:'text',pattern:'^[A-Z]{2}[0-9]{11}',placeholder:'FR0000000000'}" />
+          <InputForm @emit-value="compte.numTVA = $event" _label="Numéro de TVA" :_input="{type:'text',pattern:'^[A-Z]{2}[0-9]{11}',placeholder:'FR0000000000'}" />
         </div>
       </div>
     </div>
 
     <span class="flex align-middle justify-center items-center md:col-span-3 m-6">
-      <button type="submit" @click="formValidator()" value="submit" class="btn w-full shadow-xl">Créer le Compte</button>
+      <button @click="formValidator()" class="btn w-full shadow-xl">Créer le Compte</button>
     </span>
     
-  </form>
+  </div>
 
   <!-- Error Message -->
   <div id="msg_error" class="fixed hidden w-[75vw] right-0 bottom-[5vh] alert alert-error shadow-lg">
