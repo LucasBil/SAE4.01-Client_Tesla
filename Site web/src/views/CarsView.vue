4b9a0e77341359c3e12650a69e91632eb15a4091
Enzo Landrecy <landrece@etu.univ-smb.fr>
2023-03-27 18:38:38 +0200

    Refonte CarsView

--- Site web/src/views/CarsView.vue
+++ Site web/src/views/CarsView.vue
@@ -12,207 +12,122 @@
 
     // Composants
     import Carousel from '../components/Carousel.vue';
-    import RadioButton from '../components/RadioButton.vue';
     import BreadCrumbs from '../components/Breadcrumbs.vue';
-    import { parse } from '@vue/compiler-dom';
+import { createPinia } from 'pinia';
 
     const route = useRoute();
     const nomModele = route.params.nomModele;
+
+    // Variable Request
+    let model = ref({});
+    let motorisations = ref([{}]);
+    let type_options = ref([]);
+    let caracteristiques = ref([]);
+
+    let motorisationview = ref({}); // view Motorisation
+    let type_optionview = ref({}) // view Type Option <= Motorisation View
+    let caracteristiqueview = ref({}); // view Caractéristique <= Motorisation View
+
+    //-----------------------------
+    let selected_options = ref([]);
 
     let BreadCrumbsItems = [
         {title:'Modeles',href:'/models'},
         {title:nomModele}
     ]
 
-
-    // Variable
-    let model = ref();
-    let motorisations = ref([{}]);
-    let caracteristiques = ref([{}]);
-    let options = ref([{}]);
-    let typeoptions = ref([{}]);
-
-    let motorisationview = ref({});
-
-    if(!saves().findValue(`Caractéristiques_Mototrisation${motorisationview.value.idMotorisation}`) || !saves().findValue(`Options_Mototrisation${motorisationview.value.idMotorisation}`))
-    {
-        request().access();
-    }
-    
+    request().access();
     onMounted(async () => {
         // Get Model
         controller().ModelesController.GetByName(nomModele)
         .then((response) => {
-            model.value = Modeles.Modele.fromJson(response.data);
-
-            // Get Motorisations
+            model.value = response.data;
+            // GetAll Motorisation <= Model
             controller().MotorisationsController.GetByIdModel(model.value.idModele)
             .then((response) => {
-                motorisations.value = Modeles.Motorisation.fromJsonArray(response.data);
+                motorisations.value = response.data;
                 motorisationview.value = motorisations.value[0];
 
-                let requestsStatus = [false, false]
-                if (saves().findValue(`Caractéristiques_Mototrisation${motorisationview.value.idMotorisation}`))
-                {
-                    caracteristiques.value = saves().findValue(`Caractéristiques_Mototrisation${motorisationview.value.idMotorisation}`).value;
-                    requestsStatus[0] = true;
-                }
-                else
-                {
-                    // Get Caracteristiques
-                    controller().CaracteristiquesController.GetByIdMotorisation(motorisationview.value.idMotorisation)
+                Promise.all(motorisations.value.map(m => {
+                    // Get Options for all motorisation
+                    const promiseTY = controller().TypeOptionsController.GetByIdMotorisation(m.idMotorisation)
                     .then((response) => {
-                        caracteristiques.value = response.data;
-                        saves().save(`Caractéristiques_Mototrisation${motorisationview.value.idMotorisation}`, caracteristiques.value);
-                        requestsStatus[0] = true;
-                        if (requestsStatus[0] && requestsStatus[1])
-                            request().success(response)
+                        type_options.value.push(response.data);
                     })
                     .catch((error) => {
                         request().error(error);
                         request().debug();
                     });
-                }
 
-                // if ( saves().findValue(`TypeOptions_Mototrisation${motorisationview.value.idMotorisation}`))
-                // {
-                //     options.value = saves().findValue(`Options_Mototrisation${motorisationview.value.idMotorisation}`);
-                //     requestsStatus[1] = true;
-                // }
-                // Get Options
-                controller().TypeOptionsController.GetByIdMotorisation(motorisationview.value.idMotorisation)
-                .then((response) => {
-                    typeoptions.value = response.data;
-                    console.log(typeoptions.value)
-                    saves().save(`TypeOptions_Mototrisation${motorisationview.value.idMotorisation}`, caracteristiques.value);
-                    requestsStatus[1] = true;
-                    if (requestsStatus[0] && requestsStatus[1])
-                        request().success(response)
-                })
-                .catch((error) => {
-                    request().error(error);
-                    request().debug();
+                    const promiseCA = controller().CaracteristiquesController.GetByIdMotorisation(m.idMotorisation)
+                    .then((response) => {
+                        caracteristiques.value.push(response.data);
+                    })
+                    .catch((error) => {
+                        request().error(error);
+                        request().debug();
+                    });
+
+                    return promiseTY, promiseCA;
+                })).then(() => {
+                    type_optionview.value = type_options.value[0];
+                    DefaultOption();
+                    caracteristiqueview.value = caracteristiques.value[0];
                 });
-
+                request().success();
             })
             .catch((error) => {
-                request().error(error);
-                request().debug();
-            });
+                //console.log(error)
+                //request().error(error);
+                //request().debug();
+            })
         })
         .catch((error) => {
             request().error(error);
             request().debug();
         });
-    });
+    })
 
-    async function SwitchMotorisation(){
-        let requestsStatus = [false, false]
-
-        if(!saves().findValue(`Caractéristiques_Mototrisation${motorisationview.value.idMotorisation}`) || !saves().findValue(`Options_Mototrisation${motorisationview.value.idMotorisation}`))
-        {
-            request().access();
-        }
-
-        if (saves().findValue(`Caractéristiques_Mototrisation${motorisationview.value.idMotorisation}`))
-        {
-            caracteristiques.value = saves().findValue(`Caractéristiques_Mototrisation${motorisationview.value.idMotorisation}`).value;
-            requestsStatus[0] = true;
-        }
-        else
-        {
-            controller().CaracteristiquesController.GetByIdMotorisation(motorisationview.value.idMotorisation)
-            .then((response) => {
-                caracteristiques.value = response.data;
-                saves().save(`Caractéristiques_Mototrisation${motorisationview.value.idMotorisation}`, caracteristiques.value);
-                requestsStatus[0] = true;
-                if (requestsStatus[0] && requestsStatus[1])
-                    request().success(response)
-            })
-            .catch((error) => {
-                request().error(error);
-                request().debug();
-            });
-        }
-
-        if ( saves().findValue(`Options_Mototrisation${motorisationview.value.idMotorisation}`))
-        {
-            options.value = saves().findValue(`Options_Mototrisation${motorisationview.value.idMotorisation}`);
-            requestsStatus[1] = true;
-        }
-        else
-        {
-            controller().TypeOptionsController.GetByIdMotorisation(motorisationview.value.idMotorisation)
-            .then((response) => {
-                typeoptions.value = response.data;
-                saves().save(`TypeOptions_Mototrisation${motorisationview.value.idMotorisation}`, caracteristiques.value);
-                requestsStatus[1] = true;
-                if (requestsStatus[0] && requestsStatus[1])
-                    request().success(response)
-            })
-            .catch((error) => {
-                request().error(error);
-                request().debug();
-            });
-        }
-    }
-
-    // Price Calcul
-    let selected_options = {}
     function TotalPrice(){
         let total = 0;
         total += motorisationview.value.prix;
-        for (let [key,value] in selected_options)
-        {
-            total += value
-            print(value)
+        for (const [key,option] of selected_options.value.entries()) {
+            total += option.coutAdditionnel;
         }
         return Intl.NumberFormat('fr-FR', {  style: 'currency', currency: 'EUR' }).format(total);
     }
 
-    let checkOption = {}
-    function ToogleOption(option, event)
+    function DefaultOption()
     {
-        let value = event.checked
-        // console.log(option.idType)
-
-        //Les traveaux
-        if(value || [1,2,3].includes(option.idType))
+        for(const [TY_key,type_option] of type_optionview.value.entries())
         {
-            checkOption[event.title] = event
-            if( checkOption["4"] && checkOption["3"] )
+            for(const [TO_key,option] of type_option.optionsNavigation.entries())
             {
-                if(event.title == "3" && checkOption["4"].checked)
+                if(option.description == "De série")
                 {
-                    checkOption["4"].checked = false
-                    selected_options["4"] = 0
-                }
-                else if(event.title == "4" && checkOption["3"].checked)
-                {
-                    checkOption["3"].checked = false
-                    selected_options["3"] = 0
+                    selected_options.value[TY_key] = option;
                 }
             }
-            selected_options[event.title] = option.coutAdditionnel
         }
-        else
-        {
-            selected_options[event.title] = 0
-            checkOption[event.title] = option.coutAdditionnel
-        }
-        console.log(selected_options)
-        
     }
 
-    function Test(msg)
+    function SwitchMotorisation(key)
     {
-        console.log(msg)
+        motorisationview.value = motorisations.value[key];
+        type_optionview.value = type_options.value[key];
+        caracteristiqueview.value = caracteristiques.value[key];
+    }
+
+    function Test()
+    {
+        let first = document.getElementById("4")
+        let second = document.getElementById("5")
+        console.log(first, second);
     }
 
 </script>
 
 <template>
-    <input type="checkbox"  @click="Test($event.target)">
     <div v-if="request().requestState">
         <BreadCrumbs class="mx-6 mt-3" :_items="BreadCrumbsItems"/>
         <div class="h-[60vh] p-3">
@@ -222,10 +137,11 @@
             }"/>
         </div>
         <div class="flex m-3 gap-3 justify-center">
-            <button @click="motorisationview = motorisation; SwitchMotorisation()" v-for="motorisation in motorisations" class="btn">
+            <button @click="SwitchMotorisation(key)" v-for="motorisation,key in motorisations" class="btn">
                 {{ motorisation.nomMotorisation }}
             </button>
         </div>
+
         <div class="md:grid md:grid-cols-2 p-3 gap-3">
             <div class="flex flex-col gap-4 p-3">
                 <div class="flex flex-col gap-2">
@@ -235,7 +151,7 @@
                 <div>
                     <h2 class="text-lg font-bold">Caratéristique :</h2>
                     <div class="w-full stats stats-vertical shadow">
-                        <div v-for="caracteristique in caracteristiques" class="stat">
+                        <div v-for="caracteristique in caracteristiqueview" class="stat">
                             <div v-if="!caracteristique.description" class="stat-title">{{caracteristique.nomCaracteristique }}</div>
                             <div v-if="!caracteristique.description" class="stat-value text-primary whitespace-normal">{{ `${caracteristique.motorisationsNavigation[0].valeurCar} ${(caracteristique.unite==null)?'':caracteristique.unite}`}}</div>
                             <div v-else tabindex="0" class="collapse collapse-arrow  border-base-300 bg-base-100 rounded-box">
@@ -251,27 +167,27 @@
                 </div>
             </div>
             <div class="border-2 p-6 shadow-xl rounded-xl">
-                <h1 class="text-3xl font-bold">Prix : <span class="text-accent">{{ `${TotalPrice()}` }}</span></h1>
-                <!-- Options -->
+                <h1 class="text-3xl font-bold">Prix : <span class="text-accent"> {{ TotalPrice() }}</span></h1>
+                
                 <div class="flex flex-col my-2 gap-3">
-                    <div v-for="typeoption,key in typeoptions" class="my-2">
-                        <!-- Options sauf Autres options -->    
+                    <div v-for="typeoption,key in type_optionview" class="my-2">
+                           
                         <div v-if="typeoption.nomType != 'Autres options'">
                             <h1 class="mb-2">{{ typeoption.nomType }} :</h1>
                             <select v-model="selected_options[key]" class="select select-primary w-full">
-                                <option :title="key" @click="ToogleOption(selected_options[key], $event.target)" v-for="option in typeoption.optionsNavigation" :value="option ">{{ `${option.libelleOption} ${(option.description)?`(${option.description})`:''}` }}</option>
+                                <option v-for="option in typeoption.optionsNavigation" :value="option ">{{ `${option.libelleOption} ${(option.description)?`(${option.description})`:''}` }}</option>
                             </select>
                         </div>
-                        <!-- Autres options -->
+                        
                         <div v-else class="p-3">    
                             <h1 class="mb-2">{{ typeoption.nomType }} :</h1>
-                            <div class="flex flex-col gap-3">
+                            <div class="flex flex-col gap-3"> 
 
                                 <div v-for="option,key2 in typeoption.optionsNavigation" class="p-3">
                                     <div class="form-control">
                                         <label class="cursor-pointer label">
                                             <span class="label-text">{{ option.libelleOption }}</span> 
-                                            <input :title="key+(key2)" @change="ToogleOption(option,$event.target)" type="checkbox" class="toggle toggle-accent"/>
+                                            <input type="radio" :value="option" :id="key2" class="toggle toggle-accent"/>
                                         </label>
                                     </div>
                                     <p class="text-sm">{{ option.description }}</p>
@@ -279,9 +195,13 @@
 
                             </div>
                         </div>
+
+                        <button @click="Test()">OK</button>
+
                     </div>
                 </div>
             </div>
+            
             <div class="col-span-2 flex flex-col items-center gap-3 p-6">
                 <RouterLink @click="pdf().save(motorisationview,selected_options,TotalPrice())" class="btn w-[75%] btn-outline btn-primary" :to="{ name:'pdf'}">
                     Générer un résumé en PDF
