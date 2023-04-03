0ed98de6bf8b0b770a705859063967ae7cbdd361
LucasBil <lucas.billy@etu.univ-savoie.fr>
2023-04-03 11:31:08 +0200

    Modification controller compte

--- Site web/src/api/comptesController.js
+++ Site web/src/api/comptesController.js
@@ -7,20 +7,32 @@ class ComptesController extends ControllerManager {
         this.name = "comptes";
     }
 
+    GetAll(token) {
+        return axios.get(`${ControllerManager.baseURL}/${this.name}`, {headers : {"Authorization" : `Bearer ${token}`}});  
+    }
+
+    Put(id, data, token) {
+        return axios.put(`${ControllerManager.baseURL}/${this.name}/${id}`, data, {headers : {"Authorization" : `Bearer ${token}`}});
+    }
+
+    Delete(id, token) {
+        return axios.delete(`${ControllerManager.baseURL}/${this.name}/${id}`, {headers : {"Authorization" : `Bearer ${token}`}});
+    }
+
     GetByEmail(email) {
         return axios.get(`${ControllerManager.baseURL}/${this.name}/ByEmail/${email}`);
     }
 
-    GetIdDepartement(idDep) {
-        return axios.get(`${ControllerManager.baseURL}/${this.name}/ByIdDepartement/${idDep}`);
+    GetIdDepartement(idDep, token) {
+        return axios.get(`${ControllerManager.baseURL}/${this.name}/ByIdDepartement/${idDep}`, {headers : {"Authorization" : `Bearer ${token}`}});
     }
 
     GetByIdCB(idCB,token) {
         return axios.get(`${ControllerManager.baseURL}/${this.name}/ByIdCB/${idCB}`, {headers : {"Authorization" : `Bearer ${token}`}});
     }
 
-    GetByIdSepa(idSepa) {
-        return axios.get(`${ControllerManager.baseURL}/${this.name}/ByIdSepa/${idSepa}`);
+    GetByIdSepa(idSepa, token) {
+        return axios.get(`${ControllerManager.baseURL}/${this.name}/ByIdSepa/${idSepa}`, {headers : {"Authorization" : `Bearer ${token}`}});
     }
 
     GetToken(user)
