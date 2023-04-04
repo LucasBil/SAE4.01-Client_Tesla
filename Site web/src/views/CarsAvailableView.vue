338ee8a17f0a23b406cf89be187c7fa3e370969a
Zolkn <Landrece1503>
2023-04-04 22:24:38 +0200

    push 22:24

--- Site web/src/views/CarsAvailableView.vue
+++ Site web/src/views/CarsAvailableView.vue
@@ -4,7 +4,8 @@
     import saves from '../stores/saves.js';
     import IconFilter from '../components/icons/IconFilter.vue';
     import Card from '../components/Card.vue';
-    
+
+    let models = ref([]);
     let maxPrice = ref(0);
     let priceLimits = ref([1000000, 0]);
     let motorisations = ref();
@@ -13,23 +14,34 @@
     
     request().access();
     onMounted(async () => {
+
+        controller().ModelesController.GetAll()
+        .then((response) => {
+            models.value = response.data
+        })
+        .catch((error) => {
+            request().error(error);
+            request().debug();
+        })
+
         controller().VehiculeDemonstrationsController.GetAll()
         .then((response) => {
             motorisations.value = response.data;
-            for (let moto of motorisations.value) {
+            Promise.all(motorisations.value.map((moto) =>{
                 if (moto.motosVehiculeNavigation.prix < priceLimits.value[0]){
                     priceLimits.value[0] = moto.motosVehiculeNavigation.prix;
                 }
                 if (moto.motosVehiculeNavigation.prix > priceLimits.value[1]){
                     priceLimits.value[1] = moto.motosVehiculeNavigation.prix;
                 }
-            }
+            }))  
             maxPrice.value = priceLimits.value[1]
             request().success(response);
         })
         .catch((error) => {
             request().error(error);
         });
+
         controller().TypeOptionsController.GetAll()
         .then((response) => {
             options.value = response.data;
@@ -39,6 +51,14 @@
             request().error(error);
         });
     });
+
+    function filtre_model() {
+        let filtre = []
+        models.value.forEach(mod => {
+             mod.nomModele 
+        })
+
+    }
     
     function Test(msg)
     {
@@ -47,6 +67,7 @@
 </script>
 
 <template>
+    <input type="checkbox" @click="Test(motorisations)"/>
     <div class="drawer">
         <input id="my-drawer" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content">
@@ -55,14 +76,13 @@
                 <IconFilter class="h-full"/> Filtrer
             </label>
             <div class="md:grid md:grid-cols-3 lg:grid-cols-2">
-                <Card class="m-3" v-for="moto in motorisations" :title="moto.motosVehiculeNavigation.nomMotorisation" :resume="moto.motosVehiculeNavigation.description" :_img="moto.motosVehiculeNavigation.motorisationToOPM[0].photoOPM.url[0]" />
+                <Card class="m-3" v-for="moto in motorisations" :id="moto.idVehiculeDemo" :title="moto.motosVehiculeNavigation.nomMotorisation" :resume="moto.motosVehiculeNavigation.description" :_img="moto.motosVehiculeNavigation.motorisationToOPM[0].photoOPM.url[0]" />
             </div>
         </div> 
         <div class="drawer-side">
             <label for="my-drawer" class="drawer-overlay"></label>
             <ul class="menu p-4 w-80 bg-base-100 text-base-content">
             <!-- Sidebar content here -->
-                <h1 class="font-bold">Type Model :</h1>
                 <!-- v-for Model -->
                 <div class="form-control">
                     <label class="label cursor-pointer">
@@ -102,7 +122,7 @@
                 <h1 class="font-bold mb-3">Couleur Extérieure :</h1>
                 <button @click="Test(options)">Test</button>
                 <li>
-                    <select v-if="options.length != 0" class="select select-bordered w-full p-2" name="" id="">
+                    <select v-if="options.length != 0" class="select select-bordered w-full p-2">
                         <option value="" selected>Aucune</option>
                         <!-- v-for Color Out -->
                         <option v-for="colorOut in options[0].optionsNavigation" :value="colorOut.idOption">{{ colorOut.libelleOption }}</option>
